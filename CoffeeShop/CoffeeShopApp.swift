92a90d26fa4763d9bcc9a2eefd1b511c8e3b91d4
andre.borba <dev_andre.filipe@outlook.com>
2023-10-16 21:28:20 -0300

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by André Filipe Fonsêca Borba on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
