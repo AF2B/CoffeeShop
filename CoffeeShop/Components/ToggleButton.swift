98f6621ff349ebb203aecfee204cc38c27844fbb
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 04:56:47 -0300

    feat: toggle button

--- CoffeeShop/Components/ToggleButton.swift
+++ CoffeeShop/Components/ToggleButton.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ToggleButton: View {
+    @Binding var isDeliverSelected: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {
+                isDeliverSelected = true
+            }) {
+                Text("Deliver")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(isDeliverSelected ? Color(hex: 0xC67C4E) : Color(hex: 0xF2F2F2))
+                    .foregroundColor(isDeliverSelected ? Color.white : Color.black)
+            }
+            .cornerRadius(10)
+            
+            Button(action: {
+                isDeliverSelected = false
+            }) {
+                Text("Pick Up")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(!isDeliverSelected ? Color(hex: 0xC67C4E) : Color(hex: 0xF2F2F2))
+                    .foregroundColor(!isDeliverSelected ? Color.white : Color.black)
+            }
+            .cornerRadius(10)
+        }
     }
 }
 
+
 #Preview {
-    ToggleButton()
+    ToggleButton(isDeliverSelected: .constant(true))
 }
