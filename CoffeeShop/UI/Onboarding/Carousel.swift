dd49c997a64dd6e6beb3f86b14d4a518b4fcd0d0
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 04:56:53 -0300

    feat: carousel

--- CoffeeShop/UI/Onboarding/Carousel.swift
+++ CoffeeShop/UI/Onboarding/Carousel.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct Carousel: View {
+    @State private var selectedPageIndex = 0
+    @State private var selectedCoffee: coffeeTypes?
+    
+    let coffeeTypeArray: [coffeeTypes] = [
+        .Espresso,
+        .Cappuccino,
+        .Latte,
+        .Americano,
+        .Mocha,
+        .Macchiato,
+        .Turkish,
+        .French,
+        .ColdBrew,
+        .Affogato
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 6) {
+                ForEach(coffeeTypeArray, id: \.self) { coffeeType in
+                    Button(action: {
+                        selectedCoffee = coffeeType
+                    }) {
+                        Text(coffeeType.rawValue)
+                            .frame(width: 120, height: 50)
+                            .foregroundStyle(Color(hex: 0x2F4B4E))
+                            .background(coffeeType == selectedCoffee ? Color(UIColor(hex: 0xC67C4E)) : Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
+                    }
+                    
+                    if coffeeType != coffeeTypeArray.last {
+                        Divider()
+                            .frame(width: 5, height: 50)
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
 #Preview {
     Carousel()
+}
+
+enum coffeeTypes: String {
+    case Espresso
+    case Cappuccino
+    case Latte
+    case Americano
+    case Mocha
+    case Macchiato
+    case Turkish
+    case French
+    case ColdBrew
+    case Affogato
 }
