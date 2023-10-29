c8d573309a820a4fc84cf6989b44e1345b333c74
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 04:56:35 -0300

    feat: coffee list

--- CoffeeShop/Components/CoffeeList.swift
+++ CoffeeShop/Components/CoffeeList.swift
@@ -7,12 +7,56 @@
 
 import SwiftUI
 
-struct CoffeeListView: View {
+struct CoffeeList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topLeading) {
+            Image("coffee")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 200, height: 200)
+                .cornerRadius(16)
+            
+            HStack {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                
+                Text("4.8")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+            }
+            .padding(.top, 8)
+            .padding(.leading, 8)
+
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Cappuccino")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("with Chocolate")
+                    .foregroundColor(Color(hex: 0x9B9B9B))
+                
+                HStack(alignment: .center) {
+                    Text("$4.53")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(hex: 0x2F4B4E))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "plus")
+                        .frame(width: 44, height: 44)
+                        .background(Color(hex: 0xC67C4E))
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                        .font(.title3)
+                }
+                .padding(.top, 4)
+            }
+            .padding(.top, 202)
+        }
+        .frame(width: 200, height: 100)
     }
 }
 
 #Preview {
-    CoffeeListView()
+    CoffeeList()
 }
