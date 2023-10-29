e7b467e828e4ea0dc0098ea7bc6e5cb3af42c3fb
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 04:57:37 -0300

    feat: onboarding view

--- CoffeeShop/View/OnboardingView.swift
+++ CoffeeShop/View/OnboardingView.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea(.all)
-            VStack {
-                CoffeeBackgroundView()
-                OnboardingTitleView()
-                OnboardingParagraphView()
-                OnboardingButtonView()
+        NavigationView {
+            ZStack {
+                Image("backgroundjpg")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .center, spacing: 10) {
+                    Spacer()
+                    
+                    OnboardingTitleView()
+                    
+                    OnboardingParagraphView()
+                    
+                    NavigationLink(destination: HomeView()) {
+                        Text("Get Started")
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                            .foregroundColor(.white)
+                            .frame(width: 280, height: 70)
+                            .background(Color(UIColor(hex: 0xC67C4E)))
+                            .cornerRadius(10)
+                            .shadow(color: .black, radius: 4, x: 0, y: 4)
+                    }
+                }
+                .padding(.bottom, 32)
             }
-            .offset(y: -88)
         }
     }
 }
