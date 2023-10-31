76c7bfc245b7d318ce34d6e369bf3836a35f96cc
andre.borba <dev_andre.filipe@outlook.com>
2023-10-30 23:14:07 -0300

    fix: tabbar

--- CoffeeShop/Components/MainTabBar.swift
+++ CoffeeShop/Components/MainTabBar.swift
@@ -12,7 +12,7 @@ struct MainTabBar: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            OrderView()
+            HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
