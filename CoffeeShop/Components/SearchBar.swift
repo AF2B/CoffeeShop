ec7ea7d1e16a7bedcf663942be5fbb10fd1a1f5f
andre.borba <dev_andre.filipe@outlook.com>
2023-10-30 23:14:49 -0300

    refactor: search bar

--- CoffeeShop/Components/SearchBar.swift
+++ CoffeeShop/Components/SearchBar.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct SearchBar: View {
+    @Binding var searchText: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(Color(hex: 0xFFFFFF))
+            
+            TextField("Search", text: $searchText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundStyle(Color(hex: 0x989898))
+            
+            if !searchText.isEmpty {
+                Button(action: {
+                    searchText = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.gray)
+                        .imageScale(.medium)
+                }
+            }
+        }
+        .padding(10)
+        .background(Color(hex: 0x313131))
+        .clipShape(RoundedRectangle(cornerRadius: 18))
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    SearchBar()
+    SearchBar(searchText: .constant("Search Coffee"))
 }
