ecb4236741dc476c2ddd8007c2a1cb72f91efd17
andre.borba <dev_andre.filipe@outlook.com>
2023-11-02 21:20:56 -0300

    feat: cart view

--- CoffeeShop/View/CartView.swift
+++ CoffeeShop/View/CartView.swift
@@ -9,10 +9,47 @@ import SwiftUI
 
 struct CartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Seu Carrinho")
+                    .font(.largeTitle)
+                    .padding()
+                
+                List {
+                    Text("Café Espresso - R$2.99")
+                    Text("Cappuccino - R$3.49")
+                    Text("Latte - R$3.99")
+                    Text("Mocha - R$4.49")
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Total:")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("R$14.96")
+                        .font(.headline)
+                }
+                .padding()
+                
+                Button(action: {
+                    print("...")
+                }) {
+                    Text("Finalizar Compra")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color(hex: 0xC67C4E))
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+        }
     }
 }
-
 #Preview {
     CartView()
 }
