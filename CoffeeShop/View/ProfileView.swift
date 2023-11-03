48de32ec10bc5dc76b4217094665d494508a13bb
andre.borba <dev_andre.filipe@outlook.com>
2023-11-02 21:19:19 -0300

    docs: assets in general

---
+++ CoffeeShop/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  CoffeeShop
+//
+//  Created by André Filipe Fonsêca Borba on 02/11/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
