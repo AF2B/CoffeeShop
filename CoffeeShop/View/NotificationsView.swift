cae8c5be1b268fef2df766965eb81074b2540f85
andre.borba <dev_andre.filipe@outlook.com>
2023-11-02 21:22:08 -0300

    feat: notification view

--- CoffeeShop/View/NotificationsView.swift
+++ CoffeeShop/View/NotificationsView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct NotificationsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Notificações")
+                    .font(.largeTitle)
+                    .padding()
+                
+                List {
+                    NotificationRow(title: "Nova Promoção", message: "Desconto de 10% em todos os cafés hoje!")
+                    NotificationRow(title: "Lembrete de Pedido", message: "Seu pedido de café será entregue em breve.")
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    NotificationsView()
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
+
+struct NotificationRow: View {
+    var title: String
+    var message: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+                .font(.headline)
+            Text(message)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding(10)
+    }
 }
